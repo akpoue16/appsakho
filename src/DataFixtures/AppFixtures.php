eace6e34c8efcfe7f2b5ee9a5cfa473a68ab00ef
Akpoue Sidoine <akpoue.sidoine@gmail.com>
2022-10-22 15:56:03 +0000

    Ajout fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Client;
 use App\Entity\Dossier;
 use App\Entity\Personnel;
@@ -19,28 +20,53 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('fr_FR');
+        $genres = ['male', 'female'];
+
+        $genre = $faker->randomElements($genres);
+
         $dossier = new Dossier();
         $user = new Personnel();
         $client = new Client();
 
-        $user->setTitre("Monsieur")
-            ->setNom("KONAN")
-            ->setPrenom("Sidoine")
-            ->setEmail("admin@gmail.com")
-            ->setTel("0708052025")
-            ->setPassword($this->encoder->hashPassword($user, 'admin'));
-        
-        for($i=1; $i<10; $i++){
-            $client->setTitre("Monsieur")
-            ->setNom("KONAN")
-            ->setPrenom("Sidoine")
-            ->setEmail("admin@gmail.com")
-            ->setTel("0708052025")
+
+        if ($genre == "male") {
+            $titre = "Monsieur";
+        } else {
+            $titre = "Madame";
+        }
+        for($y = 1; $y < 5; $y++) {
+        $user->setTitre($titre)
+            ->setNom($faker->firstName($genre))
+            ->setPrenom($faker->lastName($genre))
+            ->setEmail($faker->email)
+            ->setTel($faker->mobileNumber)
+            ->setRoles(["ROLE_AVOCAT"])
             ->setPassword($this->encoder->hashPassword($user, 'admin'));
         }
 
+        for($i = 1; $i < 10; $i++) {
+            $client->setTitre($titre)
+                ->setNom($faker->firstName($genre))
+                ->setPrenom($faker->lastName($genre))
+                ->setEmail($faker->email)
+                ->setTel($faker->mobileNumber)
+                ->setRoles(["ROLE_CLIENT"])
+                ->setType("Particulier")
+                ->setPassword($this->encoder->hashPassword($user, 'password'));
+        }
+
+        for($j=1; $j < 5; $j++){
+            $dossier->setCode("Clt_00". $j)
+                ->setClient($faker->numberBetween(0, $i))
+                ->setPersonnel($faker->numberBetween(0, $y))
+                ->setCommentaire($faker->sentence(15))
+                ->setNom($faker->sentence(6));
+        }
 
         $manager->persist($user);
+        $manager->persist($client);
+        $manager->persist($dossier);
 
         // $product = new Product();
         // $manager->persist($product);
